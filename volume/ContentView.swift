eebc74bd8d6bddd08b192f3038ea8ba75d8f4d08
Butanediol <n872485238@gmail.com>
2021-07-14 15:17:41 +0800

    Initial Commit

---
+++ volume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  volume
+//
+//  Created by Butanediol on 14/7/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ volume/volumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  volumeApp.swift
+//  volume
+//
+//  Created by Butanediol on 14/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct volumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
